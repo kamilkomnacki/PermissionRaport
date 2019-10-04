b18d25ec57b3f60ebaa03c7dc56fc98410abf8b7
Kamil Komnacki <kamilkomnacki@gmail.com>
2019-10-04 21:15:36 +0200

    Send simple msg to firebase

--- app/src/main/java/com/komnacki/permissionraport/MainActivity.kt
+++ app/src/main/java/com/komnacki/permissionraport/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.komnacki.permissionraport
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true)
+        var database = FirebaseDatabase.getInstance()
+            .getReferenceFromUrl("https://permissionraport.firebaseio.com")
+
+        btn_send.setOnClickListener {
+            Log.d("MAIN:", "btn click")
+            database.push().setValue(et_email.text.toString(), "addd")
+//            database.child("test").child("1").setValue("Kamil")
+//            finish()
+        }
     }
 }
