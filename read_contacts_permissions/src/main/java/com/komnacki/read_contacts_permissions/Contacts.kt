2ceb2d3bfd397bfa8292d077ac466d5c1aa3e4a9
Kamil Komnacki <kamilkomnacki@gmail.com>
2019-10-14 21:51:45 +0200

    Get contacts from system

---
+++ read_contacts_permissions/src/main/java/com/komnacki/read_contacts_permissions/Contacts.kt
@@ -0,0 +1,48 @@
+package com.komnacki.read_contacts_permissions
+
+import android.content.ContentResolver
+import android.provider.ContactsContract
+
+class Contacts(private val contentResolver : ContentResolver) {
+
+    //todo: make private
+    fun getContacts() : ArrayList<Contact> {
+        var contacts : ArrayList<Contact> = ArrayList()
+        var cursor = contentResolver.query(
+            ContactsContract.Contacts.CONTENT_URI,
+            null,
+            null,
+            null,
+            null
+        )
+
+        if (cursor != null && cursor.count > 0) {
+            while (cursor.moveToNext()) {
+                var id : Long = cursor.getLong(cursor.getColumnIndex(ContactsContract.Contacts._ID))
+                var name : String = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                var phoneNumber : String = String()
+                if (cursor.getInt((cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+                    var pCur = contentResolver.query(
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        null,
+                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                        arrayOf(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID))),
+                        null
+                    )
+
+                    while (pCur != null && pCur.moveToNext()) {
+                        phoneNumber = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                    }
+
+                    pCur !!.close()
+                }
+                contacts.add(Contact(id, name, phoneNumber, ""))
+            }
+        }
+
+//        Log.d("READ CONTACTS" , contacts.toString())
+        return contacts
+    }
+
+
+}
