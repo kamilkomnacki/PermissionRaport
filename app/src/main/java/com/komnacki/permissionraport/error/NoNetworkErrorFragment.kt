cdb0a3073bc195579d7a3877af713956dd759715
Kamil Komnacki <kamilkomnacki@gmail.com>
2019-10-26 10:54:54 +0200

    Add no network fragment class

---
+++ app/src/main/java/com/komnacki/permissionraport/error/NoNetworkErrorFragment.kt
@@ -0,0 +1,12 @@
+package com.komnacki.permissionraport.error
+
+import androidx.fragment.app.Fragment
+
+class NoNetworkErrorFragment : Fragment() {
+    companion object {
+        fun newInstance() : NoNetworkErrorFragment {
+            return NoNetworkErrorFragment()
+        }
+    }
+
+}
