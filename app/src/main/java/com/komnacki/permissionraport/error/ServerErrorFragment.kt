3bdbc6c472c28ff08ab1c19f9345b623c3d94113
Kamil Komnacki <kamilkomnacki@gmail.com>
2019-11-12 20:32:11 +0100

    Add errors fragments

--- app/src/main/java/com/komnacki/permissionraport/error/ServerErrorFragment.kt
+++ app/src/main/java/com/komnacki/permissionraport/error/ServerErrorFragment.kt
@@ -7,15 +7,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.komnacki.permissionraport.R
 
-class NoNetworkErrorFragment : Fragment() {
+class ServerErrorFragment : Fragment() {
     companion object {
-        fun newInstance() : NoNetworkErrorFragment {
-            return NoNetworkErrorFragment()
+        fun newInstance() : ServerErrorFragment {
+            return ServerErrorFragment()
         }
     }
 
     override fun onCreateView(inflater : LayoutInflater, container : ViewGroup?, savedInstanceState : Bundle?) : View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        return inflater.inflate(R.layout.fragment_no_network_error, container, false)
+        return inflater.inflate(R.layout.fragment_server_error, container, false)
     }
 }
